f77436dc6362135f44ab564502955fe90beca0d4
o1egs <tankistonline21@gmail.com>
2023-11-10 19:10:26 +0400

    rewind, video service

---
+++ digital_breakthrough/src/services/VideoService.jsx
@@ -0,0 +1,54 @@
+// переход на таймкоды
+// drag anf drop
+
+
+
+const API_URL = ""
+
+const methods = {
+    POST: "POST",
+    GET: "GET"
+}
+
+export const saveVideo = async (file, setPercent) => {
+    let xhr = new XMLHttpRequest();
+    xhr.open(methods.POST, API_URL + "/", [true]);
+    const body = {
+
+    }
+    xhr.send([body])
+    xhr.onprogress((e) => {
+       setPercent(e.loaded)     
+    })
+}
+
+export const uploadVideo = async (setPercent) => {
+    return await fetch(API_URL + "/")
+            .then(async (response) => {
+                const reader = response.body.getReader();
+                const contentLength = +response.headers.get('Content-Length');
+                let receivedLength = 0;
+                let chunks = [];
+                while(true) {
+                    const {done, value} = await reader.read();
+
+                    if (done) {
+                        break;
+                    }
+
+                    chunks.push(value);
+                    receivedLength += value.length;
+
+                    setPercent(receivedLength / contentLength * 100);
+                }
+                let chunksAll = new Uint8Array(receivedLength);
+                let position = 0;
+                for(let chunk of chunks) {
+                    chunksAll.set(chunk, position); 
+                    position += chunk.length;
+                }
+
+                return new TextDecoder("utf-8").decode(chunksAll);}
+            )
+            .catch();
+}
